035eadfddc4ae7b32a12b6f3fdb3377c47da792f
oliveraudhie <slumantri@gmail.com>
2018-01-15 14:44:29 +0700

    man num

---
+++ index.js
@@ -0,0 +1,15 @@
+// level 0
+var numberA = 10
+let numberB = 099
+const numberC = 0.02
+const numberD = 3
+const A = Number(numberA)
+const B = Number(numberB)
+const C = Number(numberC)
+const D = Number(numberD)
+// level 1
+console.log(A + B)
+console.log(B - A)
+console.log(A * C)
+console.log(A / C)
+console.log(A % D)
